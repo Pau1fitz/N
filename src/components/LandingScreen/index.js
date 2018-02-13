29514972de13363629bd5bd12fc8638da8745e2d
Paul Fitzgerald <paulfitz99@gmail.com>
2018-02-13 20:14:31 +0000

    set up on gatsby

---
+++ src/components/LandingScreen/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LandingScreen = () => (
+  <main className="landing-screen section">
+    <div className="landing-title">
+        <h2 className="landing-text">NWA.</h2>
+    </div>
+  </main>
+)
+
+export default LandingScreen
---
+++ src/components/ThirdScreen/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const ThirdScreen = () => (         
+  <main className="third-screen section">
+    <section className="landing-title">
+        <h2 className="landing-text">We have a problem*</h2>
+        <p className="landing-subtext">*opportunity</p>
+    </section>
+
+    <section className="info-container">
+      <div className="about">
+        <p>About NWA</p>
+      </div>
+
+      <div className="details">
+        <h2 className="details-header">NWA is a business consultancy that helps people and brands do better business</h2>
+        <p className="details-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc libero tortor, dapibus eu malesuada quis, iaculis tincidunt nunc. 
+          Suspendisse congue erat a consectetur pretium. Phasellus sed congue nisl. 
+          Ut consectetur dignissim viverra. Mauris imperdiet ultrices ipsum non egestas.</p>
+      </div>
+    </section>
+  </main>
+)
+
+export default ThirdScreen
