778d1fa9760e855b51b790ea14836b06227f297c
Paul Fitzgerald <paulfitz99@gmail.com>
2018-02-14 22:38:58 +0000

    reduce size of header logo and add padding

--- src/components/HeaderLogo/index.js
+++ src/components/HeaderLogo/index.js
@@ -6,7 +6,8 @@ const HeaderLogo = ({ color }) => (<Logo color={ color }>N</Logo>)
 const Logo = styled.h1`
     font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
     color: ${ props => props.color };
-    font-size: 3em;
+    font-size: 2.5em;
+    padding-bottom: 0.5em;
     font-weight: 800;
     text-align: center;
 `
